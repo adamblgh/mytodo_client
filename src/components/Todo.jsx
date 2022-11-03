97eb10746afabb67ace8a42b7605f203e5d5c3a7
Your Name <you@example.com>
2022-11-03 12:26:07 +0100

    todokesz

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,45 +1,63 @@
 import React,{useState} from 'react'
 import ListGroup  from 'react-bootstrap/ListGroup';
-import { getTodos } from './getData';
-import {useQuery} from 'react-query';
-
-
+import { getTodos,delItem,delItems,addItem } from './getData';
+import {useQuery,useQueryClient,useMutation, QueryClient} from 'react-query';
+ 
+ 
 export const Todo=()=> {
-  const {data,status}=useQuery("todos",getTodos)
-  status == 'success' && console.log(data.data)
-
-  const handleAdd=()=>{
-    console.log("insert")
-    //setTodos([...todos,newTodo])
-  }
-  const handleDelete=(id)=>{
-    console.log("törlés:",id)
-    /*const newArr=todos.filter(item=>item!=value)
-    setTodos(newArr)*/
-  }
-  /*const handleDone=(obj)=>{
-    console.log(obj.nextElementSibling)
-    obj.nextElementSibling.classList.toggle('text-decoration-line-through')
-  }*/
-
+  const [newItem,setNewItem] = useState('')
+  const {data,isLoading}=useQuery("todos",getTodos)
+  !isLoading && console.log(data.data)
+ 
+  const clientQuery = useQueryClient()
+ 
+  const mutationDel = useMutation(delItem,{
+    onSuccess:()=>{
+      clientQuery.invalidateQueries("todos")
+    }
+  })
+ 
+  const mutationDelAll = useMutation(delItems,{
+    onSuccess:()=>{
+      clientQuery.invalidateQueries("todos")
+    }
+  })
+ 
+  const mutationAdd = useMutation(addItem,{
+    onSuccess:()=>{
+      setNewItem('')
+      clientQuery.invalidateQueries("todos")
+    }
+  })
+ 
+ 
   return (
+    <>
+    {isLoading?<p>is loading...</p>:
     <div className='todo'>
       <h1 className='text-center'>My todos</h1>
       <form className='d-flex justify-content-evenly m-1 p-2'>
-        <input type="text"  onChange={(e)=>console.log(e.target.value)} />
-        <i className="fa-solid fa-plus fa-2x m-1 text-success" onClick={handleAdd}></i>
-        <i className="fa-solid fa-trash text-danger fa-2x" onClick={()=>console.log("Összes törlés")}></i>
+        <input type="text"  onChange={(e)=>setNewItem(e.target.value)} value={newItem} />
+ 
+        <i className="fa-solid fa-plus fa-2x m-1 text-success" onClick={()=>mutationAdd.mutate({name:newItem})}></i>
+ 
+        <i className="fa-solid fa-trash text-danger fa-2x" onClick={()=>mutationDelAll.mutate()}></i>
       </form>
       <ListGroup>
-        {status=='success'&& data.data.map(item=>
+        {data.data.map(item=>
         <ListGroup.Item className='d-flex justify-content-between' key={item.id}>
-          <i className="fa-solid fa-square-check text-success fa-2x" onClick={(e)=>console.log(item.id)}></i>
-          <span className="">{item.name}</span>
-          <i className="fa-solid fa-trash text-danger fa-2x" onClick={(e)=>handleDelete(item.id)}></i>
+          <i className={item.status? "fa-solid fa-square-check text-success fa-2x":"fa-solid fa-square-check text-secondary fa-2x"} 
+          onClick={()=>console.log(item.id)}></i>
+          <span className={item.status? "text-decoration-line-through":""}>
+            {item.name}</span>
+          <i className="fa-solid fa-trash text-danger fa-2x" 
+          onClick={()=>mutationDel.mutate(item.id)}></i>
           </ListGroup.Item>)}
         </ListGroup>
-        
-      
+ 
+ 
     </div>
+        }
+        </>
   )
 }
--- src/components/getData.jsx
+++ src/components/getData.jsx
@@ -5,3 +5,18 @@ export const getTodos = async () => {
     const response = await axios.get(url+'/all');
     return await response;
 };
+
+export const delItem = async (id) => {
+    const response = await axios.delete(url+'/delete/'+id);
+    return await response;
+};
+
+export const delItems = async () => {
+    const response = await axios.delete(url+'/deleteall/');
+    return await response;
+};
+
+export const addItem = async (formdata) => {
+    const response = await axios.post(url+'/add',formdata);
+    return await response;
+};
